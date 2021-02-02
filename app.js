60341b88b122aff68824a5fedfd58438d62055d2
dstearle <dallas.st.earle@gmail.com>
2021-02-02 13:22:43 -0800

    Set port to 3000 in main app

--- app.js
+++ app.js
@@ -18,3 +18,9 @@ app.use(express.static(__dirname + '/public'));
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define Port
+const port = 3000;
+
+// Start Server
+const server = app.listen(port, () => console.log(`Server started on port ${ port }`));
