dc18d545f5de58d8610d59a69b124f888fca2843
dstearle <dallas.st.earle@gmail.com>
2021-02-02 14:28:32 -0800

    Created event and listener for sending a text message when clicking the submit button

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,36 @@ const textInput = document.getElementById('msg'),
 const button = document.getElementById('button'),
 // The input for the phone number
 const respone = document.querySelector('.response');
+
+// Event listener for the submit button
+button.addEventListener('click', send, flase);
+
+// Sends a text message from the inputs
+function send() {
+
+    // The phone number to be texted
+    const number = numberInput.nodeValue.replace(/\/D/g, '');
+
+    // The text message to send
+    const text = textInput.value;
+
+    // Fetches the data to be sent
+    fetch('/', {
+
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({number: number, text: text})
+
+    })
+    .then(function(res) {
+
+        console.log(res);
+
+    })
+    .catch(function(err) {
+        
+        console.log(err)
+    
+    })
+
+}
